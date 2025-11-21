ae0cd5d46d5310479ad2481d2ad2977b312404d9
JMH-security <j.mike.hefner@gmail.com>
2025-11-21 08:14:49 -0600

    misc

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,62 +7,62 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import authClient from "@/lib/authdb";
 
 export const {
-	handlers: { GET, POST },
-	signIn,
-	signOut,
-	auth,
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+  auth,
 } = NextAuth({
-	adapter: MongoDBAdapter(authClient),
-	providers: [
-		MicrosoftEntraID({
-			clientId: process.env.ENTRA_APP_ID,
-			clientSecret: process.env.ENTRA_SECRET,
-			allowDangerousEmailAccountLinking: true,
-			authorization: {
-				params: {
-					scope: "openid profile email User.Read",
-					redirect_uri: process.env.ENTRA_ID_AUTH_URL,
-					tenantId: process.env.ENTRA_TENANT_ID,
-				},
-			},
-		}),
-		Google({
-			clientId: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_SECRET,
-			profile(profile) {
-				return { role: profile.role ?? "G-user", ...profile };
-			},
-			authorization: {
-				params: {
-					prompt: "consent",
-					access_type: "offline",
-					response_type: "code",
-				},
-			},
-		}),
-	],
-	session: {
-		strategy: "database",
-	},
-	callbacks: {
-		async signIn({ user }) {
-			// Restrict sign-in to users with specific email domains
-			const allowedDomains = [
-				"securityengineersinc.com",
-				"seiteam.com",
-				"gmail.com",
-				"solustream.com",
-			];
-			const userEmail = user?.email as string;
-			const userDomain = userEmail.split("@")[1];
-			const userAllowed = allowedDomains.includes(userDomain);
-			return userAllowed;
-		},
-		async session({ session }) {
-			if (session.user) {
-				console.log("Session User Callback:", session.user.name);
-			}
-			return session;
-		},
-	},
+  adapter: MongoDBAdapter(authClient),
+  providers: [
+    MicrosoftEntraID({
+      clientId: process.env.ENTRA_APP_ID,
+      clientSecret: process.env.ENTRA_SECRET,
+      allowDangerousEmailAccountLinking: true,
+      authorization: {
+        params: {
+          scope: "openid profile email User.Read",
+          redirect_uri: process.env.ENTRA_ID_AUTH_URL,
+          tenantId: process.env.ENTRA_TENANT_ID,
+        },
+      },
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      profile(profile) {
+        return { role: profile.role ?? "G-user", ...profile };
+      },
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  session: {
+    strategy: "database",
+  },
+  callbacks: {
+    async signIn({ user }) {
+      // Restrict sign-in to users with specific email domains
+      const allowedDomains = [
+        "securityengineersinc.com",
+        "seiteam.com",
+        "gmail.com",
+        "solustream.com",
+      ];
+      const userEmail = user?.email as string;
+      const userDomain = userEmail.split("@")[1];
+      const userAllowed = allowedDomains.includes(userDomain);
+      return userAllowed;
+    },
+    async session({ session }) {
+      if (session.user) {
+        console.log("Session User Callback:", session.user.name);
+      }
+      return session;
+    },
+  },
 });
--- src/lib/authdb.ts
+++ src/lib/authdb.ts
@@ -5,33 +5,34 @@
 //***********************************************************/
 
 import { MongoClient, ServerApiVersion } from "mongodb";
+import { auth } from "./auth";
 
 const uri = process.env.CONNECTIONSTRING || "";
 const options = {
-	serverApi: {
-		version: ServerApiVersion.v1,
-		strict: true,
-		deprecationErrors: true,
-	},
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 };
 
 if (!uri) {
-	throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
 
 let authClient: MongoClient;
 
 if (process.env.NODE_ENV === "development") {
-	let globalWithMongo = global as typeof globalThis & {
-		_mongoClient?: MongoClient;
-	};
+  let globalWithMongo = global as typeof globalThis & {
+    _mongoClient?: MongoClient;
+  };
 
-	if (!globalWithMongo._mongoClient) {
-		globalWithMongo._mongoClient = new MongoClient(uri, options);
-	}
-	authClient = globalWithMongo._mongoClient;
+  if (!globalWithMongo._mongoClient) {
+    globalWithMongo._mongoClient = new MongoClient(uri, options);
+  }
+  authClient = globalWithMongo._mongoClient;
 } else {
-	authClient = new MongoClient(uri, options);
+  authClient = new MongoClient(uri, options);
 }
 
 export default authClient;
