c1b1c3d86a60c588a2b88c972408ba9b93786ed9
JMH-security <j.mike.hefner@gmail.com>
2026-01-07 12:47:08 -0600

    local job

--- src/app/api/jobs/[id]/route.ts
+++ src/app/api/jobs/[id]/route.ts
@@ -4,40 +4,40 @@ import dbConnect from "@/lib/db";
 import LocalJob from "@/models/LocalJob";
 
 export async function GET(
-	request: Request,
-	{ params }: { params: { id: string } }
+  request: Request,
+  { params }: { params: { id: string } }
 ) {
-	await dbConnect();
-	const prm = await params;
-	const localJob = await LocalJob.findById(prm.id);
-	if (!localJob)
-		return NextResponse.json({ error: "Not found" }, { status: 404 });
-	return NextResponse.json(localJob);
+  await dbConnect();
+  const prm = await params;
+  const localJob = await LocalJob.findById(prm.id);
+  if (!localJob)
+    return NextResponse.json({ error: "Not found" }, { status: 404 });
+  return NextResponse.json(localJob);
 }
 
 export async function PUT(
-	request: Request,
-	{ params }: { params: { id: string } }
+  request: Request,
+  { params }: { params: { id: string } }
 ) {
-	await dbConnect();
-	const data = await request.json();
-	const prm = await params;
-	const localJob = await LocalJob.findByIdAndUpdate(prm.id, data, {
-		new: true,
-	});
-	if (!localJob)
-		return NextResponse.json({ error: "Not found" }, { status: 404 });
-	return NextResponse.json(localJob);
+  await dbConnect();
+  const data = await request.json();
+  const prm = await params;
+  const localJob = await LocalJob.findByIdAndUpdate(prm.id, data, {
+    new: true,
+  });
+  if (!localJob)
+    return NextResponse.json({ error: "Not found" }, { status: 404 });
+  return NextResponse.json(localJob, { status: 200 });
 }
 
 export async function DELETE(
-	request: Request,
-	{ params }: { params: { id: string } }
+  request: Request,
+  { params }: { params: { id: string } }
 ) {
-	await dbConnect();
-	const prm = await params;
-	const localJob = await LocalJob.findByIdAndDelete(prm.id);
-	if (!localJob)
-		return NextResponse.json({ error: "Not found" }, { status: 404 });
-	return NextResponse.json({ message: "Deleted" });
+  await dbConnect();
+  const prm = await params;
+  const localJob = await LocalJob.findByIdAndDelete(prm.id);
+  if (!localJob)
+    return NextResponse.json({ error: "Not found" }, { status: 404 });
+  return NextResponse.json({ message: "Deleted" });
 }
